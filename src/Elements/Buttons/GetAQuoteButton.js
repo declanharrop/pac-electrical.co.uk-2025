a6dc450469b7a990a6171a23ffaa2fd6c63a9832
Declan Harrop <dh@dhweb.dev>
2025-01-15 11:15:45 +0000

    added stuff

--- src/Elements/Buttons/GetAQuoteButton.js
+++ src/Elements/Buttons/GetAQuoteButton.js
@@ -6,7 +6,12 @@ export default function GetAQuoteButton() {
       <button
         type="button"
         className="button-alt"
-        style={{ fontWeight: '600', letterSpacing: '1px', fontSize: '1.4rem' }}
+        style={{
+          fontWeight: '600',
+          letterSpacing: '1px',
+          fontSize: '1.4rem',
+          boxShadow: '0 0 6px 0px var(--light-grey)',
+        }}
       >
         GET A QUOTE <span>→</span>
       </button>
