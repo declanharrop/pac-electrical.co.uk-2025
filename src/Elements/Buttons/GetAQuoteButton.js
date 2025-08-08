e2f3a710a5262d277a7bf070a68cc525f3d74996
Declan Harrop <dh@dhweb.dev>
2025-08-08 11:52:22 +0100

    Changed get a quote button

--- src/Elements/Buttons/GetAQuoteButton.js
+++ src/Elements/Buttons/GetAQuoteButton.js
@@ -7,9 +7,15 @@ export default function GetAQuoteButton() {
         type="button"
         className="button-alt"
         style={{
+          backgroundColor: 'var(--green)',
+          border: 'var(--green)',
+          color: 'white',
           fontWeight: '600',
           letterSpacing: '1px',
-          fontSize: '1.4rem',
+          height: '50px',
+          borderRadius: '50px',
+          width: '200px',
+          fontSize: '1.7rem',
           boxShadow: '0 0 6px 0px var(--light-grey)',
         }}
       >
